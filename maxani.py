91d58066450af67ee995435f70c7512f21f1f48a
ino8090 <ibrahirahimhalil@gmail.com>
2026-08-26 06:07:01 +0300

    Update logo scaling height in maxani.py

--- maxani.py
+++ maxani.py
@@ -206,7 +206,7 @@ def start_m3u_stream():
             filter_str = (
                 '[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,'
                 'pad=1920:1080:(ow-iw)/2:(oh-ih)/2:black,fps=30[main];'
-                '[1:v]scale=-2:80[logo];'
+                '[1:v]scale=-2:100[logo];'
                 '[main][logo]overlay=55:55[v]'
             )
             logo_input = ['-i', 'logo.png']
