47dd6f270c12dc2ae6b35096a4168e0989254496
ino8090 <ibrahirahimhalil@gmail.com>
2026-08-30 20:59:14 +0300

    Update logo scaling and overlay parameters

--- maxpremier.py
+++ maxpremier.py
@@ -219,23 +219,25 @@ def start_m3u_stream():
 
         logo_inputs = []
         
-        # Filtre zinciri: Her iki logo için de height=60px ölçeklendirme yapıldı
+        # Filtre zinciri:
+        # Sol üst logo: yükseklik 80px (scale=-2:80), overlay=50:50
+        # Sağ üst logo: yükseklik 30px (scale=-2:30), overlay=main_w-overlay_w-50:50
         if has_logo1 and has_logo2:
             logo_inputs = ['-i', 'logo.png', '-i', 'logo2.png']
             filter_str = (
                 '[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,'
                 'pad=1920:1080:(ow-iw)/2:(oh-ih)/2:black,fps=30[main];'
-                f'[{logo1_input_index}:v]scale=-2:60[logo1];'
-                f'[{logo2_input_index}:v]scale=-2:60[logo2];'
-                '[main][logo1]overlay=40:40[tmp];'
-                '[tmp][logo2]overlay=main_w-overlay_w-40:40[v]'
+                f'[{logo1_input_index}:v]scale=-2:80[logo1];'
+                f'[{logo2_input_index}:v]scale=-2:30[logo2];'
+                '[main][logo1]overlay=50:50[tmp];'
+                '[tmp][logo2]overlay=main_w-overlay_w-50:50[v]'
             )
         elif has_logo1:
             logo_inputs = ['-i', 'logo.png']
             filter_str = (
                 '[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,'
                 'pad=1920:1080:(ow-iw)/2:(oh-ih)/2:black,fps=30[main];'
-                f'[{logo1_input_index}:v]scale=-2:79[logo1];'
+                f'[{logo1_input_index}:v]scale=-2:80[logo1];'
                 '[main][logo1]overlay=50:50[v]'
             )
         elif has_logo2:
@@ -244,13 +246,13 @@ def start_m3u_stream():
                 '[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,'
                 'pad=1920:1080:(ow-iw)/2:(oh-ih)/2:black,fps=30[main];'
                 f'[{logo2_input_index}:v]scale=-2:30[logo2];'
-                '[main][logo2]overlay=main_w-overlay_w-49:49[v]'
+                '[main][logo2]overlay=main_w-overlay_w-50:50[v]'
             )
         else:
             logo_inputs = []
             filter_str = (
                 '[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,'
-                'pad=1920:1080:(ow-iw)/2:(oh-ih)/2:black,fps=30[v]'
+                'pad=1920:1080:(oh-ih)/2:black,fps=30[v]'
             )
 
         command = [
