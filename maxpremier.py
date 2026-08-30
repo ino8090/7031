bbb93ae5aca6138b14ed8d5476f08532e5f6bef5
ino8090 <ibrahirahimhalil@gmail.com>
2026-08-30 20:41:35 +0300

    Change default STREAM_KEY from 'maxnimasyon' to 'maxpremier'

--- maxpremier.py
+++ maxpremier.py
@@ -11,7 +11,7 @@ import requests
 
 # ===================== AYARLAR =====================
 RTMP_URL = "rtmp://ssh101.bozztv.com:1935/ssh101"
-STREAM_KEY = os.getenv("STREAM_KEY") or "maxnimasyon"
+STREAM_KEY = os.getenv("STREAM_KEY") or "maxpremier"
 RTMP_SERVER = f"{RTMP_URL}/{STREAM_KEY}"
 
 M3U_URL = os.getenv("M3U_URL") or "https://raw.githubusercontent.com/ino8090/0101/refs/heads/main/mpremiuum.m3u"
